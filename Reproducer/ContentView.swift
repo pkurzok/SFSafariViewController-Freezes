0f70c1183ac505b3e4d1e60294a9076147a1b585
Peter Kurzok <peter.kurzok@maibornwolff.de>
2024-10-15 17:31:42 +0200

    Reproduce Issue

--- Reproducer/ContentView.swift
+++ Reproducer/ContentView.swift
@@ -5,17 +5,37 @@
 //  Created by Peter Kurzok on 15.10.24.
 //
 
+import SafariServices
 import SwiftUI
 
+// MARK: - ContentView
+
 struct ContentView: View {
+    @State private var showSafari = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Open Website") {
+                showSafari = true
+            }
+            .sheet(isPresented: $showSafari) {
+                SafariView(url: URL(string: "https://www.apple.com")!)
+            }
         }
-        .padding()
+    }
+}
+
+// MARK: - SafariView
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        // No need to update the view controller in this case
     }
 }
 
