dedcbc95444bc47e6ffb202940ff523cc6aa1b97
Peter Kurzok <peter.kurzok@maibornwolff.de>
2024-10-15 17:13:42 +0200

    Initial Commit

---
+++ Reproducer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reproducer
+//
+//  Created by Peter Kurzok on 15.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reproducer/ReproducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReproducerApp.swift
+//  Reproducer
+//
+//  Created by Peter Kurzok on 15.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ReproducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
